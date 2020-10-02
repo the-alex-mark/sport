278db6b3d8abe0dfdccff6d06f86f1682ea495a3
Makarov_AM <makarov_am@stack.stack-it.ru>
2020-10-02 21:26:54 +0300

    Обновление

---
+++ wp-content/mu-plugins/includes/classes/sport-class-organization.php
@@ -0,0 +1,19 @@
+<?php
+
+if (!defined('ABSPATH'))
+    exit;
+
+/**
+ * 
+ */
+class Organization {
+
+    /**
+     * 
+     */
+    public function __construct() {
+
+    }
+}
+
+return new Organization();
